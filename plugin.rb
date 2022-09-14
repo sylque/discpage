97c6f15a305e64dec76f8a1094a5275aacb7920b
sylque <sylvain.quendez@gmail.com>
2022-09-14 14:09:59 +0200

    Fix for issue #32

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discpage
 # about: Create static pages and attach discussions to them
-# version: 1.0.51
+# version: 1.0.52
 # authors: Sylvain Quendez
 # url: https://github.com/sylque/discpage
 
