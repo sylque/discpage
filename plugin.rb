60614f06c9e83d903aeab20319b060e12903d2bd
sylque <sylvain.quendez@gmail.com>
2024-01-01 17:48:36 +0100

    deploy

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discpage
 # about: Create static pages and attach discussions to them
-# version: 1.0.57
+# version: 1.0.58
 # authors: Sylvain Quendez
 # url: https://github.com/sylque/discpage
 
